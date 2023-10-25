e22fed32bbada7ef164b9f7b5f13e8999d200306
wenboliu-mcmaster <74749597+wenboliu-mcmaster@users.noreply.github.com>
2023-10-25 09:53:57 -0400

    inital

---
+++ wiki.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "os"
+	"log"
+    "net/http"
+)
+type Page struct {
+    Title string
+    Body  []byte
+}
+func main() {
+    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    p1.save()
+    p2, _ := loadPage("TestPage")
+    fmt.Println(string(p2.Body))
+}
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return os.WriteFile(filename, p.Body, 0600)
+}
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
